f0a87edb3f5382dcf55f6d9b7a958d59002fcf98
Peter Szakács <peter.szakacs@student.tuke.sk>
2017-05-02 09:43:22 +0200

    fixed state evaluation

--- Assets/scripts/unattachableScripts/StandardStateEvaluator.cs
+++ Assets/scripts/unattachableScripts/StandardStateEvaluator.cs
@@ -8,19 +8,35 @@ namespace Assets.scripts.unattachableScripts
 {
     class StandardStateEvaluator : IGameStateEvaluator {
 
+        spawner[] spawners;
+
+        public StandardStateEvaluator()
+        {
+            spawners = UnityEngine.Object.FindObjectsOfType<spawner>();
+        }
+
         public GameState TestGameState(GameState CurrentState)
         {
+            bool SpawnersFinished = true;
+            foreach (spawner spw in spawners)
+            {
+                bool SpawnerFinished = (spw.count == 0);
+                SpawnersFinished &= SpawnerFinished;
+            }
+
             GameObject PlayerBase = GameObject.FindGameObjectWithTag("PlayerBase");
             if (PlayerBase == null)
             {
                 return GameState.LOST;
             }
+
             GameObject[] PlayerTanks = GameObject.FindGameObjectsWithTag("Player");
             bool ArePlayersAlive = false;
             foreach (GameObject PlayerTank in PlayerTanks)
             {
                 ArePlayersAlive |= PlayerTank.activeInHierarchy;
             }
+
             if (!ArePlayersAlive)
             {
                 return GameState.LOST;
@@ -28,7 +44,7 @@ namespace Assets.scripts.unattachableScripts
             else
             {
                 GameObject[] EnemyTanks = GameObject.FindGameObjectsWithTag("EnemyTank");
-                if (EnemyTanks.Length == 0)
+                if (EnemyTanks.Length == 0 && SpawnersFinished)
                 {
                     return GameState.WON;
                 }
